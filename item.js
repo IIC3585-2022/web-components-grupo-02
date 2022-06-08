db7a18c8c5776954bf58a33c193b269ad0ba237b
micea <micea@uc.cl>
2022-06-07 21:11:50 -0400

    first commit

---
+++ item.js
@@ -0,0 +1,144 @@
+class item extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({mode: 'open'});
+        this.nombre;
+        this.precio;
+        this.precio_real;
+        this.calificacion;
+        this.descuento;
+        this.img;
+    }
+
+    static get observedAttributes(){
+        return ['nombre','precio', 'precio_real', 'calificacion', 'descuento', 'img'];
+    }
+
+    attributeChangedCallback(nameAtr, oldValue, newValue){
+        switch(nameAtr){
+            case 'nombre':
+                this.nombre = newValue;
+            break;
+            case 'precio':
+                this.precio = newValue;
+            break;
+            case 'precio_real':
+                this.precio_real = newValue;
+            break;
+            case 'calificacion':
+                this.calificacion = newValue;
+            break;
+            case 'descuento':
+                this.descuento = newValue;
+            break;
+            case 'img':
+                this.img = newValue;
+            break;
+        }
+    }
+
+    connectedCallback(){
+        this.shadowRoot.innerHTML = `
+        <style>
+            html {
+                box-sizing: border-box;
+                font-size: 62.5%; /* 1REM = 10px */
+            }
+            *, *:before, *:after {
+                box-sizing: inherit;
+            }
+            body {
+                font-size: 1.6rem;
+                line-height: 1.5;
+            }
+            img { /* img responsive */
+                max-width: 100%; /* Para que solo ocupe el ancho del contenedor */
+                display: block; /* Para que no quede espacio debajo de la imagen */
+                height: auto;
+            }
+            .max-width {
+                max-width: 22rem;
+            }
+            .container {
+                background-color: #fff;
+                border: .1rem solid #e6e6e6;
+                border-top: .2rem solid #e6e6e6;
+                display: flex;
+                flex-direction: column;
+                border-radius: 2rem;
+            }
+            .image-item img {
+                border-top-left-radius: 2rem;
+                border-top-right-radius: 2rem;
+            }
+            .text-item {
+                border-top: .1rem solid #e6e6e6;
+            }
+            .text-item > p {
+                font-size: 1.8rem;
+                text-align: center;
+                margin: .8rem 0 0 0;
+                line-height: 1.2;
+            }
+            .precio-items {
+                display: flex;
+                justify-content: space-around;
+            }
+            .precio-items p {
+                font-size: 1.4rem;
+                margin: .5rem 0;
+            }
+            .precio {
+                font-size: 1.6rem;
+                color: #389FDF;
+            }
+            .precio-items span {
+                text-decoration: line-through;
+            }
+            .descuento {
+                background-color: #BD1D50;
+                padding: .1rem .5rem;
+                border-radius: .5rem;
+                color: white;
+            }
+            .raiting {
+                display: flex;
+                align-items: center;
+                margin-bottom: .5rem;
+            }
+            .raiting img {
+                width: 2rem;
+                height: 2rem;
+                margin-right: .5rem;
+            }
+            .raiting p {
+                margin: 0;
+            }
+        </style>
+
+        <div class="container max-width">
+            <div class="image-item">
+                <img loading="lazy" src="${this.img}" alt="Imagen Producto">
+            </div>
+            <div class="text-item">
+                <p>${this.nombre}</p>
+                <div class="precio-items">
+                    <div>
+                        <p class="precio">$${this.precio}</p>
+                        <p class="normal">Normal: <span>$${this.precio_real}</span></p>
+                        <div class="raiting">
+                            <img loading="lazy" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Estrella_amarilla.png/800px-Estrella_amarilla.png" alt="Imagen rating">
+                            <p>${this.calificacion}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="descuento">-${this.descuento}%</p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>`; 
+    }
+}
+
+window.customElements.define('sell-item-component', item);
---
+++ list.js
@@ -0,0 +1,180 @@
+class list extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({mode: 'open'});
+        this.promt;
+        this.titulo;
+        this.item1;
+        this.item2;
+        this.item3;
+        this.number = 4;
+    }
+
+    static get observedAttributes(){
+        return ['item1', 'item2', 'item3', 'promt', 'titulo', 'number'];
+    }
+
+    attributeChangedCallback(nameAtr, oldValue, newValue){
+        switch(nameAtr){
+            case 'item1':
+                this.item1 = newValue;
+            break;
+            case 'item2':
+                this.item2 = newValue;
+            break;
+            case 'item3':
+                this.item3 = newValue;
+            break;
+            case 'promt':
+                this.promt = newValue;
+            break;
+            case 'titulo':
+                this.titulo = newValue;
+            break;
+            case 'number':
+                this.number = newValue;
+            break;
+        }
+    }
+
+    agregar(){
+        this.name = this.shadowRoot.querySelector("input").value;
+        this.shadowRoot.querySelector("input").value = "";
+        if (this.name == '') {
+            alert("No puedes dejar el campo vacío");
+        } 
+        else {
+            this.all_items = this.shadowRoot.querySelector(`.items`);
+            // Create div
+            this.div = document.createElement("div");
+            this.div.id = `item${this.number}`;
+            this.div.classList.add("item");
+            // Create p
+            this.p = document.createElement("p");
+            this.p.innerHTML = this.name;
+            // Create button
+            this.delete_button = document.createElement("button");
+            this.delete_button.id = `item${this.number}`;
+            this.delete_button.classList.add("remove-button");
+            this.delete_button.innerHTML = `<img src="img/remove.png" alt="imagen eliminar">`;
+            this.delete_button.type = "button";
+            // Add the event listener
+            this.delete_button.addEventListener('click', evt => {
+                console.log(this.delete_button.id);
+                this.eliminar(this.delete_button.id);
+            });
+            // Append elements
+            this.div.appendChild(this.p);
+            this.div.appendChild(this.delete_button);
+            // Append element
+            this.all_items.appendChild(this.div);
+
+            this.number++;
+        }
+    }
+
+    eliminar(id){
+        this.remove_div = this.shadowRoot.querySelector(`#${id}`);
+        this.remove_div.remove();
+    }
+
+    connectedCallback(){
+        this.shadowRoot.innerHTML = `
+        <style>
+            html {
+                box-sizing: border-box;
+                font-size: 62.5%; /* 1REM = 10px */
+            }
+            *, *:before, *:after {
+                box-sizing: inherit;
+            }
+            body {
+                font-size: 1.6rem;
+                line-height: 1.5;
+            }
+            img { /* img responsive */
+                max-width: 100%; /* Para que solo ocupe el ancho del contenedor */
+                display: block; /* Para que no quede espacio debajo de la imagen */
+                height: auto;
+                width: 1.2rem;
+            }
+            h1 {
+                margin: 0;
+            }
+            button {
+                border: none;
+                background: none;
+            }
+            .items {
+                display: flex;
+                flex-direction: column;
+                align-items: left;
+                margin-left: 2rem;
+            }
+            .items p {
+                margin: .5rem 0;
+            }
+            .items .item:last-child {
+                margin-bottom: 1rem;
+            }
+            .item {
+                display: flex;
+                align-items: center;
+            }
+            .item p {
+                flex-basis: 8rem;
+            }
+            .item img {
+                margin-left: 1rem;
+            }
+            .agregar {
+                display: flex;
+            }
+            .agregar p {
+                margin: .1rem 0;
+            }
+            .agregar input {
+                margin-left: .5rem;
+            }
+        </style>
+        <div>
+            <h1>${this.titulo}</h1>
+            <div class="items">
+                <div id="item1" class="item">
+                    <p>${this.item1}</p>
+                    <button id="item1" class="remove-button" type="button"><img src="img/remove.png" alt="imagen eliminar"></button>
+                </div>
+                <div id="item2" class="item">
+                    <p>${this.item2}</p>
+                    <button id="item2" class="remove-button" type="button"><img src="img/remove.png" alt="imagen eliminar"></button>
+                </div>
+                <div id="item3" class="item">
+                    <p>${this.item3}</p>
+                    <button id="item3" class="remove-button" type="button"><img src="img/remove.png" alt="imagen eliminar"></button>
+                </div>
+            </div>
+            <div class="agregar">
+                <p>${this.promt}:</p>
+                <input type="text">
+                <button class="add-button" type="button"><img src="img/add.png" alt="imagen agregar"></button>
+            </div>
+        </div>`;
+        
+        this.add_button = this.shadowRoot.querySelector(".add-button");
+        this.add_button .addEventListener('click', evt => {
+            this.agregar();
+        });
+        this.remove_buttons = this.shadowRoot.querySelectorAll(".remove-button");
+        this.remove_buttons.forEach(remove_button => {
+            remove_button.addEventListener('click', evt => {
+                this.eliminar(remove_button.id);
+            });
+        });
+    }
+    
+    disconnectedCallback() {
+        this.add_button.removeEventListener("click", this);
+    }
+}
+
+window.customElements.define('list-item-component', list);
